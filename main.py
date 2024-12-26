b0c6bf6931a67dff63ea421be9ed11cb53c9ffb1
Rostislav Galiv <rostix@roprojects.ru>
2024-12-27 02:41:20 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import dns
 import dns.resolver
-import socket
-import requests
-from ip2geotools.databases.noncommercial import DbIpCity
-from geopy.distance import distance
 
-def printDetails(ip):
-    res = DbIpCity.get(ip, api_key="free")
-    print(f"Location: {res.city}, {res.region}, {res.country}")
 
 print('Get Advanced MailBox Info.\nType Mail Address here:')
 mbox = input()
@@ -19,6 +12,4 @@ for rdata in answers:
     answers = dns.resolver.resolve(rdata.exchange, 'A')
     print('Host', rdata.exchange, 'has preference', rdata.preference)
     for rdata in answers:
-        response = requests.get(f'https://ipapi.co/{rdata.address}/json/').json()
-        print(f'Host IP: {rdata.address}\nServer Geo Info:')
-        printDetails(rdata.address)
+        print(f'Host IP: {rdata.address}')
